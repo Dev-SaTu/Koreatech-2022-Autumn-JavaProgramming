06db155216282d3b068b845c0a02ce0c039d13e6
Dev-SaTu <tkdwns1610@naver.com>
2022-09-22 09:34:45 +0900

    20220922

---
+++ src/JavaPractice02_01.java
@@ -0,0 +1,95 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class JavaPractice02_01 {
+
+	public static void main(String[] args) {
+
+		// 56 7 12 34 75 86 23 65 12 6
+		
+		/*
+		 * #1
+		 */
+
+		System.out.print("input: ");
+
+		Scanner scan = new Scanner(System.in);
+
+		int[] array = new int[10];
+
+		for (int i = 0; i < array.length; i++) {
+
+			array[i] = scan.nextInt();
+
+		}
+
+		for (int i = 0; i < array.length; i++) {
+
+			for (int j = i + 1; j < array.length; j++) {
+
+				if (array[i] >= array[j]) {
+
+					array[i] ^= array[j];
+					array[j] ^= array[i];
+					array[i] ^= array[j];
+
+				}
+
+			}
+
+		}
+
+		for (int i = 0; i < array.length; i++) {
+
+			System.out.printf("%d ", array[i]);
+
+		}
+
+		System.out.println();
+
+		/*
+		 * #2
+		 */
+
+		int[][] matrix = Make2DArray(5);
+
+		System.out.printf("%d, %d\n", matrix.length, matrix[0].length);
+
+		/*
+		 * #3
+		 */
+
+		System.out.print("input: ");
+
+		Scanner scan2 = new Scanner(System.in);
+
+		try {
+
+			System.out.println(1 / scan2.nextInt());
+
+		} catch (ArithmeticException e) {
+
+			if (e.getMessage().contains("zero")) {
+
+				System.out.println("0으로 나눌 수 없습니다.");
+
+			}
+
+		} catch (InputMismatchException e) {
+
+			System.out.println("정수가 아닙니다.");
+
+		}
+
+		scan.close();
+		scan2.close();
+		
+	}
+
+	static int[][] Make2DArray(int num) {
+
+		return new int[num][num];
+
+	}
+
+}
---
+++ src/JavaPractice02_02.java
@@ -0,0 +1,56 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class JavaPractice02_02 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * #1
+		 */
+
+		int r = new Random().nextInt(101);
+
+		System.out.println(r);
+		
+		Scanner scan = null; 
+
+		boolean win = false;
+
+		for (int i = 0; i < 7; i++) {
+
+			System.out.print("input: ");
+
+			scan = new Scanner(System.in);
+
+			int x = scan.nextInt();
+
+			if (x > r) {
+
+				System.out.println("추측한 숫자가 생성된 숫자보다 높다.");
+
+			} else if (x < r) {
+
+				System.out.println("추측한 숫자가 생성된 숫자보다 낮다.");
+
+			} else {
+
+				System.out.println("승리!");
+
+				win = true;
+				
+				break;
+
+			}
+
+		}
+
+		if (!win) {
+
+			System.out.println("패배!");
+
+		}
+
+	}
+
+}
